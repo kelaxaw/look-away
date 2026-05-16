8f512b2d9508d5f0510f2b6a0516aacc1fe942b9
kelaxaw1337 <ke1axaw1337@gmail.com>
2026-05-16 19:33:17 +0300

    Initial Commit

---
+++ look away/look_awayApp.swift
@@ -0,0 +1,17 @@
+//
+//  look_awayApp.swift
+//  look away
+//
+//  Created by Vasiliy Smirnov on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct look_awayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
